262f0669c1bbd60e02cbb424393391820dc0f00a
Shubhu0500 <shubhu0500@gmail.com>
2025-04-19 11:23:03 +0530

    deploying app

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const path = require('path');
+const fs = require('fs');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
@@ -8,6 +10,9 @@ const mongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -50,6 +55,17 @@ const fileFilter = (req, file, cb) => {
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  {
+    flags: 'a',
+  }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // db.execute('SELECT * FROM products')
 //   .then((result) => {
@@ -121,7 +137,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_uri)
   .then((result) => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch((err) => {
     console.log(err);
